a52f93155942f1871f1e99a798d8d1544b72f0ce
Graham Tackley <graham.tackley@guardian.co.uk>
2010-09-10 15:58:27 +0100

    Use proguard plugin to generate uber jar

--- project/build/MicroserverProject.scala
+++ project/build/MicroserverProject.scala
@@ -1,11 +1,17 @@
 import sbt._
 
-class MicroserverProject(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
+class MicroserverProject(info: ProjectInfo) extends DefaultProject(info)
+        with IdeaProject
+        with ProguardProject {
   val commonsIo = "commons-io" % "commons-io" % "1.4" withSources()
 
-  val jetty = "org.mortbay.jetty" % "jetty" % "6.1.24"
-  val jettyUtil = "org.mortbay.jetty" % "jetty-util" % "6.1.24"
+  val jetty = "org.mortbay.jetty" % "jetty" % "6.1.24" intransitive()
+  val jettyUtil = "org.mortbay.jetty" % "jetty-util" % "6.1.24" intransitive()
 
   val servletApi = "javax.servlet" % "servlet-api" % "2.5" withSources()
+
+  override def proguardOptions = List(
+    "-keep public class com.gu.microserver.MicroServer { public static void main(java.lang.String[]); }",
+    "-keep class org.mortbay.**")
 }
 
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,6 +1,8 @@
 import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val repo = "GH-pages repo" at "http://mpeltonen.github.com/maven/"
+  val ideaRepo = "GH-pages repo" at "http://mpeltonen.github.com/maven/"
+
   val idea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.1-SNAPSHOT"
+  val proguard = "org.scala-tools.sbt" % "sbt-proguard-plugin" % "0.0.4"
 }
 
