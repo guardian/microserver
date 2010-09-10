18b21a2af55aa669c4ed660d99f19292c40ec53e
Graham Tackley <graham.tackley@guardian.co.uk>
2010-09-10 15:27:34 +0100

    Converted to sbt project
    No other changes, other than switching to sbt

---
+++ project/build/Microserver.scala
@@ -0,0 +1,6 @@
+import sbt._
+
+class MicroserverProject(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
+
+}
+
--- src/main/java/com/gu/microserver/MicroServer.java
+++ src/main/java/com/gu/microserver/MicroServer.java
@@ -9,9 +9,9 @@ public class MicroServer {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         WebAppContext root = new WebAppContext(server,"/", null);
-        root.addServlet(new ServletHolder(new PageServlet("com/gu/microserver/templates/article.html")), "/article");
-        root.addServlet(new ServletHolder(new PageServlet("com/gu/microserver/templates/front.html")), "/front");
-        root.addServlet(new ServletHolder(new PageServlet("com/gu/microserver/templates/wide-resource.html")), "/resource");
+        root.addServlet(new ServletHolder(new PageServlet("templates/article.html")), "/article");
+        root.addServlet(new ServletHolder(new PageServlet("templates/front.html")), "/front");
+        root.addServlet(new ServletHolder(new PageServlet("templates/wide-resource.html")), "/resource");
         root.addServlet(new ServletHolder(new ResourceServlet("text/css", "cssUrl")), "/css");
         root.addServlet(new ServletHolder(new ResourceServlet("text/javascript", "javascriptUrl")), "/js");
         server.start();
