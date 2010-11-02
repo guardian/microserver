8b9888e60adbccc116cf5712b39b2b22d5a29670
Grant Klopper <grant.klopper@guardian.co.uk>
2010-11-02 10:25:31 +0000

    Made port configurable
    sbt>run --port=8081

--- src/main/java/com/gu/microserver/MicroServer.java
+++ src/main/java/com/gu/microserver/MicroServer.java
@@ -7,7 +7,10 @@ import org.mortbay.jetty.webapp.WebAppContext;
 public class MicroServer {
 
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
+
+        Config config = getConfiguration(args);
+        
+        Server server = new Server(config.port);
         WebAppContext root = new WebAppContext(server,"/", null);
         root.addServlet(new ServletHolder(new PageServlet("templates/article.html")), "/article");
         root.addServlet(new ServletHolder(new PageServlet("templates/front.html")), "/front");
@@ -18,4 +21,20 @@ public class MicroServer {
         server.start();
     }
 
+    private static Config getConfiguration(String[] args) {
+        Config config = new Config();
+        if (args != null) {
+            for (String arg : args) {
+                if (arg.startsWith("--port=")) {
+                    config.port = Integer.parseInt(arg.replace("--port=", ""));
+                }
+            }
+        }
+        return config;
+    }
+
+    private static class Config {
+        //  --port=8080
+        int port = 8080;
+    }
 }
